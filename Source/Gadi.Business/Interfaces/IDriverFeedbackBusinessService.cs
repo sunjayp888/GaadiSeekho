de4138bc4c71916a992b34617a9261f5d28fa499
paradkarsh24 <paradkarsh24@gmail.com>
2018-07-30 21:19:27 +0530

    Business Code

---
+++ Source/Gadi.Business/Interfaces/IDriverFeedbackBusinessService.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Gadi.Common.Dto;
+using Gadi.Business.Models;
+using Gadi.Data.Entity;
+
+namespace Gadi.Business.Interfaces
+{
+    public interface IDriverFeedbackBusinessService
+    {
+        //Create
+        Task<ValidationResult<DriverFeedback>> CreateDriverFeedback(DriverFeedback driverFeedback);
+
+        //Retrieve
+        Task<DriverFeedback> RetrieveDriverFeedback(int driverFeedbackId);
+        Task<PagedResult<DriverFeedback>> RetrieveDriverFeedbacks(List<OrderBy> orderBy = null, Paging paging = null);
+        //Task<PagedResult<DriverGrid>> Search(string term, List<OrderBy> orderBy = null, Paging paging = null);
+    }
+}
---
+++ Source/Gadi.Business/Interfaces/IDrivingSchoolBusinessService.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Gadi.Common.Dto;
+using Gadi.Business.Models;
+using Gadi.Data.Entity;
+
+namespace Gadi.Business.Interfaces
+{
+    public interface IDrivingSchoolBusinessService
+    {
+        //Create
+        Task<ValidationResult<DrivingSchool>> CreateDrivingSchool(DrivingSchool drivingSchool);
+
+        //Retrieve
+        Task<DrivingSchool> RetrieveDrivingSchool(int drivingSchoolId);
+        Task<PagedResult<DrivingSchool>> RetrieveDrivingSchools(List<OrderBy> orderBy = null, Paging paging = null);
+        //Task<PagedResult<DrivingSchoolGrid>> Search(string term, List<OrderBy> orderBy = null, Paging paging = null);
+    }
+}
