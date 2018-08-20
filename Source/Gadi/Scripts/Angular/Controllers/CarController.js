aa0fc694fd2221d8399a113d17c7f44c3266154e
paradkarsh24 <paradkarsh24@gmail.com>
2018-08-20 17:53:37 +0530

    Angular Controller, Service and Index Completed.

---
+++ Source/Gadi/Scripts/Angular/Controllers/CarController.js
@@ -0,0 +1,72 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('Gadi')
+        .controller('CarController', CarController);
+
+    CarController.$inject = ['$window', 'CarService', 'Paging', 'OrderService', 'OrderBy', 'Order'];
+
+    function CarController($window, CarService, Paging, OrderService, OrderBy, Order) {
+        /* jshint validthis:true */
+        var vm = this;
+        vm.cars = [];
+        vm.paging = new Paging;
+        vm.pageChanged = pageChanged;
+        vm.orderBy = new OrderBy;
+        vm.order = order;
+        vm.orderClass = orderClass;
+        vm.searchCars = searchCars;
+        vm.searchKeyword = "";
+        vm.searchMessage = "";
+        vm.retrieveCars = retrieveCars;
+        vm.initialise = initialise;
+
+        function initialise() {
+            order("Name");
+        }
+
+        function retrieveCars() {
+            return CarService.retrieveCars(vm.paging, vm.orderBy)
+                .then(function (response) {
+                    vm.cars = response.data.Items;
+                    vm.paging.totalPages = response.data.TotalPages;
+                    vm.paging.totalResults = response.data.TotalResults;
+                    vm.searchMessage = vm.cars.length === 0 ? "No Records Found" : "";
+                    return vm.cars;
+                });
+        }
+
+        function searchCars(searchKeyword) {
+            vm.searchKeyword = searchKeyword;
+            return CarService.searchCar(vm.searchKeyword, vm.paging, vm.orderBy)
+                .then(function (response) {
+                    vm.cars = response.data.Items;
+                    vm.paging.totalPages = response.data.TotalPages;
+                    vm.paging.totalResults = response.data.TotalResults;
+                    vm.searchMessage = vm.cars.length === 0 ? "No Records Found" : "";
+                    return vm.cars;
+                });
+        }
+
+        function pageChanged() {
+            if (vm.searchKeyword) {
+                return searchCars(vm.searchKeyword)();
+            }
+            return retrieveCars();
+        }
+
+        function order(property) {
+            vm.orderBy = OrderService.order(vm.orderBy, property);
+            if (vm.searchKeyword) {
+                return searchCars(vm.searchKeyword)();
+            }
+            return retrieveCars();
+        }
+
+        function orderClass(property) {
+            return OrderService.orderClass(vm.orderBy, property);
+        }
+
+    }
+})();
---
+++ Source/Gadi/Scripts/Angular/Controllers/DriverFeedbackController.js
@@ -0,0 +1,72 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('Gadi')
+        .controller('DriverFeedbackController', DriverFeedbackController);
+
+    DriverFeedbackController.$inject = ['$window', 'DriverFeedbackService', 'Paging', 'OrderService', 'OrderBy', 'Order'];
+
+    function DriverFeedbackController($window, DriverFeedbackService, Paging, OrderService, OrderBy, Order) {
+        /* jshint validthis:true */
+        var vm = this;
+        vm.driverFeedbacks = [];
+        vm.paging = new Paging;
+        vm.pageChanged = pageChanged;
+        vm.orderBy = new OrderBy;
+        vm.order = order;
+        vm.orderClass = orderClass;
+        vm.searchDriverFeedbacks = searchDriverFeedbacks;
+        vm.searchKeyword = "";
+        vm.searchMessage = "";
+        vm.retrieveDriverFeedbacks = retrieveDriverFeedbacks;
+        vm.initialise = initialise;
+
+        function initialise() {
+            order("Name");
+        }
+
+        function retrieveDriverFeedbacks() {
+            return DriverFeedbackService.retrieveDriverFeedbacks(vm.paging, vm.orderBy)
+                .then(function (response) {
+                    vm.driverFeedbacks = response.data.Items;
+                    vm.paging.totalPages = response.data.TotalPages;
+                    vm.paging.totalResults = response.data.TotalResults;
+                    vm.searchMessage = vm.driverFeedbacks.length === 0 ? "No Records Found" : "";
+                    return vm.driverFeedbacks;
+                });
+        }
+
+        function searchDriverFeedbacks(searchKeyword) {
+            vm.searchKeyword = searchKeyword;
+            return DriverFeedbackService.searchDriverFeedback(vm.searchKeyword, vm.paging, vm.orderBy)
+                .then(function (response) {
+                    vm.driverFeedbacks = response.data.Items;
+                    vm.paging.totalPages = response.data.TotalPages;
+                    vm.paging.totalResults = response.data.TotalResults;
+                    vm.searchMessage = vm.driverFeedbacks.length === 0 ? "No Records Found" : "";
+                    return vm.driverFeedbacks;
+                });
+        }
+
+        function pageChanged() {
+            if (vm.searchKeyword) {
+                return searchDriverFeedbacks(vm.searchKeyword)();
+            }
+            return retrieveDriverFeedbacks();
+        }
+
+        function order(property) {
+            vm.orderBy = OrderService.order(vm.orderBy, property);
+            if (vm.searchKeyword) {
+                return searchDriverFeedbacks(vm.searchKeyword)();
+            }
+            return retrieveDriverFeedbacks();
+        }
+
+        function orderClass(property) {
+            return OrderService.orderClass(vm.orderBy, property);
+        }
+
+        }
+})();
